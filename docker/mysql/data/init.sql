c966e9872b02ce4595fa0fb29a51cba5ab1254c1
NAKAI ZEN <kd1293049@st.kobedenshi.ac.jp>
2024-05-23 10:34:45 +0900

    sql変更

--- docker/mysql/data/init.sql
+++ docker/mysql/data/init.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: 127.0.0.1
--- 生成日時: 2024-05-13 14:28:01
+-- 生成日時: 2024-05-23 10:31:59
 -- サーバのバージョン： 10.4.28-MariaDB
 -- PHP のバージョン: 8.2.4
 
@@ -41,24 +41,12 @@ CREATE TABLE `answers` (
 --
 
 INSERT INTO `answers` (`answer_id`, `user_id`, `text`, `quetion_id`, `image_filename`, `bestans`) VALUES
-(1, 2, 'いいと思います', 1, NULL, 0),
-(2, 1, '出来てるやん', 3, NULL, 0),
-(3, 1, 'テスト', 3, NULL, 0),
-(4, 1, 'テスト', 1, NULL, 0),
-(5, 1, 'いいですね', 1, NULL, 0),
-(6, 1, 'ああああ', 16, NULL, 1),
-(7, 1, 'aaa', 17, NULL, 0),
-(8, 1, 'いいですね', 17, NULL, 0),
-(9, 1, 'こんにちは', 17, NULL, 0),
-(10, 10, 'いいね', 3, NULL, 0),
-(11, 11, '神戸です', 19, NULL, 0),
-(12, 11, '神戸でした', 19, NULL, 0),
-(13, 12, '神戸ですか', 19, NULL, 1),
-(14, 12, 'aaa?', 20, NULL, 1),
-(15, 12, 'ほんまにな', 18, NULL, 1),
-(16, 12, 'SimpleAdapterを使用した場合は不可能です\r\nsetItemBitmapを使用したら普通のImageViewには表示できるはずです', 23, NULL, 0),
-(17, 12, '出来ません', 23, NULL, 0),
-(18, 11, 'https://qiita.com/aymikmts/items/7139fa6c4da3b57cb4fc\r\nBitmapを普通の画像に変換してみては？？↑のサイトを参考にしてみてください。', 23, NULL, 1);
+(25, 17, 'DockerでLAMP環境を構築し、マイページから画像を変更してその画像を保存し、元の画像を削除するには、以下の手順を実行します。ここでは、Docker Composeを使用してLAMPスタックをセットアップし、PHPスクリプトを使用して画像のアップロードと削除を行います。', 27, NULL, 0),
+(26, 19, 'まずは、Visual Studioを使って新しいWindows Formsアプリケーションを作成します。これは、お料理の準備みたいなものですね。お鍋とフライパンを出しておきましょう。', 26, NULL, 0),
+(27, 21, '5つも思い浮かばないです。', 29, NULL, 0),
+(28, 19, 'int：整数型。32ビットの符号付き整数を表します。\r\ndouble：倍精度浮動小数点型。64ビットの浮動小数点数を表します。\r\nbool：ブール型。真（true）または偽（false）を表します。\r\nchar：文字型。1つのUnicode文字を表します。\r\nstring：文字列型。文字のシーケンス（文字列）を表します。\r\nなどでどうでしょうか？？', 29, NULL, 1),
+(29, 21, '受け入れる他ないかもしれません', 30, NULL, 0),
+(30, 19, 'htmlspecialchars()を使えば問題なく表示出来ますよ！', 30, NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -71,7 +59,7 @@ CREATE TABLE `article` (
   `article_title` varchar(255) NOT NULL,
   `user_id` int(11) NOT NULL,
   `section_title` mediumtext DEFAULT NULL,
-  `section_text` varchar(255) NOT NULL,
+  `section_text` text NOT NULL,
   `tag` varchar(255) NOT NULL,
   `good` int(255) NOT NULL DEFAULT 0,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
@@ -83,19 +71,10 @@ CREATE TABLE `article` (
 --
 
 INSERT INTO `article` (`article_id`, `article_title`, `user_id`, `section_title`, `section_text`, `tag`, `good`, `created_at`, `updated_at`) VALUES
-(1, '', 1, NULL, '作りました', '', 0, '2024-04-25 05:56:34', '2024-05-01 00:53:08'),
-(2, 'てすととうこう', 3, 'はじめに//つぎに//', 'てすとです//てすです//', 'test', 1, '2024-04-30 02:51:48', '2024-05-02 00:40:08'),
-(3, 'てすととうこう', 3, 'はじめに//つぎに//', 'てすとです//てすです//', 'test', 2, '2024-04-30 02:53:18', '2024-05-02 00:40:11'),
-(4, 'てすととうこう', 3, 'はじめに//', 'てすとです//', 'test', 1, '2024-04-30 02:53:22', '2024-05-02 00:39:59'),
-(5, 'てすととうこう', 3, 'はじめに//', 'てすとです//', 'test', 1, '2024-04-30 02:53:34', '2024-05-02 00:40:01'),
-(6, 'テスト', 12, '作り方//コード//', 'このアプリでは//...//', 'C#', 1, '2024-05-01 00:38:55', '2024-05-02 00:40:02'),
-(7, 'PHPでSQLを使用したサイトの作成方法', 10, 'はじめに//コード//', '今回はPHPでSQLを使用して、データベースからデータの取得をしたり、データベースを更新することのできるアプリの説明をしていこうと思います。//<?php $sort = null; include \"header.php\"; if (!isset($article)) {     require_once __DIR__ . \'/class/article.php\';     $article = new article(); } if(isset($_POST[\'sort\'])){     $sort', 'PHP、SQL', 0, '2024-05-13 00:43:22', '2024-05-13 00:43:22'),
-(8, 'PHPでSQLを使用したサイトの作成方法', 10, 'はじめに//コード//', '今回はPHPでSQLを使用して、データベースからデータの取得をしたり、データベースを更新することのできるアプリの説明をしていこうと思います。// public function __construct()     {         $loadIni = new LoadIni();          // PDOオブジェクトを生成する 															         //$dsn = \'mysql:host=localhost;dbname=teamc;charset=utf8\';   ', 'PHP、SQL', 0, '2024-05-13 00:44:54', '2024-05-13 00:44:54'),
-(9, '', 10, '//', ' public function __construct()     {         $loadIni = new LoadIni();          // PDOオブジェクトを生成する 															         //$dsn = \'mysql:host=localhost;dbname=teamc;charset=utf8\';         //$user = \'kobe\';         //$password = \'denshi\';         $dsn = \'', '', 0, '2024-05-13 00:46:16', '2024-05-13 00:46:16'),
-(10, '', 10, '//', ' public function __construct()     {         $loadIni = new LoadIni();          // PDOオブジェクトを生成する 															         //$dsn = \'mysql:host=localhost;dbname=teamc;charset=utf8\';         //$user = \'kobe\';         //$password = \'denshi\';         $dsn = \'', '', 0, '2024-05-13 00:46:17', '2024-05-13 00:46:17'),
-(11, 'PHP', 10, 'はじめに//', '//', '', 0, '2024-05-13 00:46:45', '2024-05-13 00:46:45'),
-(12, 'PHP', 10, 'はじめに//コード//', '今回はPHPでSQLを使用して、データベースからデータの取得をしたり、データベースを更新することのできるアプリの説明をしていこうと思います。//<php//', 'PHP、SQL', 0, '2024-05-13 00:47:19', '2024-05-13 00:47:19'),
-(13, '', 10, '//', '//', '', 0, '2024-05-13 00:47:39', '2024-05-13 00:47:39');
+(19, 'Kotlinでrealmを使用してデータを管理する', 17, 'はじめに//Realmを実装する//', '今回はKotlinでrealmを使用してデータの管理を行いました。どんなアプリでもデータを保存をできた方が便利ですよね！！//まずはbuild.gradleのdependenciesに以下のコードを記載します\r\nimplementation \'io.realm:android-adapters:2.1.1\'\r\npluginsには\r\nid \'realm-android\'\r\nを記載します\r\n//', 'なし', 0, '2024-05-22 02:30:45', '2024-05-22 02:32:36'),
+(20, 'Pythonのインストール', 19, 'はじめに//Pythonのインストール//初めてのPythonプログラム//基本的なデータ型//', 'こんにちは、みなさん。今日はPythonというプログラミング言語の基礎についてお話しします。Pythonはとても人気があり、初心者にも優しい言語ですので、ぜひ一緒に学んでいきましょうね。//まずはPythonをインストールしましょう。公式サイトから最新のPythonをダウンロードしてインストールしてくださいね。\r\n\r\n1.Python公式サイトにアクセス\r\n2.\"Downloads\"セクションからお使いのOSに合ったバージョンをダウンロード\r\n3.インストーラーを実行して、インストールを完了させます//Pythonのインストールが終わったら、さっそくプログラムを書いてみましょう。簡単なプログラムとして、「Hello, World!」を表示するものを書きます。コードは以下の通りです：\r\nprint(\"Hello, World!\")\r\nこれをエディタに入力して、ファイル名を hello.py として保存します。ターミナルやコマンドプロンプトで次のコマンドを実行すると、プログラムが動きますよ：\r\n画面に \"Hello, World!\" と表示されれば成功です！//Pythonにはいくつかの基本的なデータ型があります。これらを使って様々なデータを扱うことができますよ。\r\n\r\n整数型 (int): 整数を扱います。例: a = 5\r\n浮動小数点型 (float): 小数点を含む数を扱います。例: b = 3.14\r\n文字列型 (str): テキストを扱います。例: c = \"こんにちは\"\r\nリスト型 (list): 複数の値をまとめて扱います。例: d = [1, 2, 3]//', 'なし', 1, '2024-05-22 03:04:22', '2024-05-23 01:10:14'),
+(21, 'C#の基礎について', 22, 'はじめに//変数とデータ型//演算子//制御構文//', 'C#（シーシャープ）は、Microsoftが開発したオブジェクト指向のプログラミング言語で、.NETフレームワーク上で動作します。以下にC#の基礎的な概念と構文を説明します。//C#では、変数を宣言して値を格納するために、データ型を指定します。基本的なデータ型には、int（整数）、double（浮動小数点数）、bool（真偽値）、char（文字）、string（文字列）などがあります。//C#には、算術演算子（+、-、*、/、%）、比較演算子（==、!=、<、>、<=、>=）、論理演算子（&&、||、!）などがあります。//C#には、条件分岐やループを処理するための制御構文があります。\r\nif文\r\nif (age >= 18) {\r\n    Console.WriteLine(\"You are an adult.\");\r\n} else {\r\n    Console.WriteLine(\"You are a minor.\");\r\n}\r\n\r\nswitch文\r\nswitch (grade) {\r\n    case \'A\':\r\n        Console.WriteLine(\"Excellent!\");\r\n        break;\r\n    case \'B\':\r\n        Console.WriteLine(\"Good!\");\r\n        break;\r\n    default:\r\n        Console.WriteLine(\"Keep trying!\");\r\n        break;\r\n}\r\n\r\nforループ\r\nfor (int i = 0; i < 10; i++) {\r\n    Console.WriteLine(i);\r\n}\r\n\r\nwhileループ\r\nint count = 0;\r\nwhile (count < 5) {\r\n    Console.WriteLine(count);\r\n    count++;\r\n}//', 'なし', 0, '2024-05-23 00:52:29', '2024-05-23 00:52:29'),
+(22, 'SQLの四大命令について', 23, 'はじめに//SELECT文//INSERT文//UPDATE文//DELETE文//最後に//', 'たくさんのデータを使う上でデータベースは必要不可欠ですよね！\r\nそんなデータベースにデータを登録や更新、取り出しから削除まで行う事ができるのがSQL文です。\r\nそんなSQLの基礎である四大命令について今回は学んで行きましょう！！//SELECT文はデータベースからデータを取得します。\r\n書き方は以下の通りです。\r\nSELECT * FROM Employees;//INSERTは新しいデータをテーブルに挿入します。\r\n書き方は以下の通りです。\r\nINSERT INTO Employees (Name, Age, Department) VALUES (\'John Doe\', 30, \'HR\');//UPDATEは既存のデータを更新します。\r\n書き方は以下の通りです。\r\nUPDATE Employees SET Age = 31 WHERE Name = \'John Doe\';//DELETEはデータを削除します。\r\n書き方は以下の通りです。\r\nDELETE FROM Employees WHERE Name = \'John Doe\';//今回は簡単になりましたが、次回はもう少し踏み込んだことも解説していきたいと思っております。\r\n是非この記事のグッドをお願い致します。//', 'なし', 2, '2024-05-23 01:05:42', '2024-05-23 01:22:21');
 
 -- --------------------------------------------------------
 
@@ -107,7 +86,7 @@ CREATE TABLE `comments` (
   `comment_id` int(11) NOT NULL,
   `article_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `text` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `text` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `image_filename` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `review` int(5) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -117,15 +96,12 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`comment_id`, `article_id`, `user_id`, `text`, `image_filename`, `review`) VALUES
-(1, 4, 10, 'さ', NULL, NULL),
-(2, 5, 10, 'ささ', NULL, NULL),
-(3, 5, 10, 'sasa', NULL, NULL),
-(4, 5, 10, 'sasa', NULL, NULL),
-(5, 5, 14, 'ｗｗｗｗｗ', NULL, NULL),
-(6, 5, 14, 'ｗｗｗｗ', NULL, 4),
-(7, 6, 14, 'aaaaaaaaaaaa', NULL, 4),
-(8, 6, 14, 'aaaaaaa', NULL, 5),
-(9, 7, 10, 'エラーおきててわろた', NULL, 1);
+(20, 19, 17, '追記：これで実装は完了です。次は実際に使用していこうと思います。気になる方は私のプロフィールから次の記事に飛んでみてください。\r\n', NULL, 5),
+(21, 20, 19, '参考になりました。', NULL, 4),
+(22, 20, 19, 'などのコメントお待ちしております。', NULL, 3),
+(23, 20, 19, 'プロフィールをご覧ください。', NULL, 3),
+(24, 21, 20, '私は最近C#を始めたばかりなのでとても参考になりました！！', NULL, 5),
+(25, 21, 21, '演算子のところがホントに分からなかったです。', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -144,30 +120,9 @@ CREATE TABLE `goodarticle` (
 --
 
 INSERT INTO `goodarticle` (`article_id`, `user_id`, `good`) VALUES
-(2, 0, 1),
-(1, 0, 1),
-(1, 4, 0),
-(2, 4, 1),
-(1, 5, 1),
-(2, 5, 0),
-(1, 6, 0),
-(2, 6, 0),
-(1, 7, 0),
-(1, 8, 1),
-(2, 8, 0),
-(1, 9, 1),
-(2, 9, 0),
-(1, 10, 0),
-(2, 10, 1),
-(5, 12, 0),
-(3, 12, 1),
-(2, 12, 1),
-(4, 12, 1),
-(1, 12, 1),
-(5, 10, 1),
-(4, 10, 1),
-(3, 10, 1),
-(6, 10, 1);
+(20, 17, 1),
+(22, 17, 1),
+(22, 20, 1);
 
 -- --------------------------------------------------------
 
@@ -178,7 +133,7 @@ INSERT INTO `goodarticle` (`article_id`, `user_id`, `good`) VALUES
 CREATE TABLE `question` (
   `question_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `text` varchar(255) NOT NULL,
+  `text` text NOT NULL,
   `tag` varchar(255) NOT NULL,
   `image_filename` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
@@ -190,21 +145,10 @@ CREATE TABLE `question` (
 --
 
 INSERT INTO `question` (`question_id`, `user_id`, `text`, `tag`, `image_filename`, `created_at`, `updated_at`) VALUES
-(1, 1, '作りたいです', 'C#', NULL, '2024-04-24 05:43:28', '2024-04-24 05:43:28'),
-(2, 1, 'AIを活用する', 'Python', NULL, '2024-04-24 05:43:28', '2024-04-24 05:43:28'),
-(3, 1, '初めまして！！！神戸電子専門学校の学生に向けた情報共有サイトの作成を現在行っているのですが、○○が中々上手く行かず苦戦しています。テキストの文字数が多すぎると...に変更出来るようにすることが現在取り組んでいる課題です。', 'C#', NULL, '2024-04-24 05:43:28', '2024-04-24 05:43:28'),
-(12, 1, '今回作ろうと思っている物は....', 'C#', NULL, '2024-04-24 05:43:28', '2024-04-24 05:43:28'),
-(13, 1, '今作っているショッピングサイトに以下の機能を追加したいです。\r\n\r\n・カート\r\n・ユーザー\r\n・ジャンル検索\r\n\r\nこのうちジャンル検索だけ上手く出来ませんでした。', 'PHP', '0J01018 (2).png', '2024-04-24 05:43:28', '2024-04-24 05:43:28'),
-(14, 1, 'Pythonで作成したいです', 'Python', NULL, '2024-04-24 05:43:28', '2024-04-24 05:43:28'),
-(15, 4, 'a', 'aa', NULL, '2024-04-24 05:43:28', '2024-04-24 05:43:28'),
-(16, 10, '神戸太郎です', '神戸', NULL, '2024-04-24 05:43:28', '2024-04-24 05:43:28'),
-(17, 10, 'こんにちは', 'C#', NULL, '2024-04-24 05:54:55', '2024-04-24 05:54:55'),
-(18, 10, 'aa', 'a', NULL, '2024-04-25 05:49:16', '2024-04-25 05:49:16'),
-(19, 10, 'インプット\r\nこんにちは\r\n私はC#\r\nPythonのKotlin\r\nそのPHPは私のHTMLですか？\r\nいいえrubyです。SQL!?なに？\r\nこのC++\r\nそんな私のCSS\r\nです', 'C++', NULL, '2024-04-26 04:54:24', '2024-05-01 00:46:38'),
-(20, 10, 'aa', 'a', NULL, '2024-04-30 00:34:54', '2024-04-30 00:34:54'),
-(21, 12, 'テスト', 'C#,C,Python', NULL, '2024-05-01 00:30:21', '2024-05-01 00:30:21'),
-(22, 12, 'てすとのとうこう', 'あ', NULL, '2024-05-01 00:36:59', '2024-05-01 00:36:59'),
-(23, 10, 'KotlinでBitmapの画像をリストビューに表示させたいのですがどのようにすれば解決できますかね？？', 'Kotlin', NULL, '2024-05-13 04:34:43', '2024-05-13 04:34:43');
+(26, 17, '初めまして！\r\n今回私が作成しているC#のアプリケーションなのですが、テキストファイルに保存したデータをリストボックスに表示させたいです。', 'なし', NULL, '2024-05-22 02:04:31', '2024-05-22 02:04:31'),
+(27, 18, 'DockerでLAMP環境を構築し、マイページから画像を変更させその画像をvolumeに保存し、元になった画像を削除したい。', 'なし', NULL, '2024-05-22 02:40:57', '2024-05-22 02:40:57'),
+(29, 20, 'C#初心者です。\r\nC#の基本データ型を5つ挙げてください。', 'なし', NULL, '2024-05-23 00:43:55', '2024-05-23 00:43:55'),
+(30, 24, 'PHPで質問投稿サイトを作成しているのですが\r\n質問内で<h1>と</h1>でくくった時に文字が大きくなってしまいます。\r\nどうすれば回避出来ますか？？？', 'なし', NULL, '2024-05-23 01:26:19', '2024-05-23 01:27:04');
 
 -- --------------------------------------------------------
 
@@ -226,20 +170,14 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `email`, `name`, `password`, `icon_filename`, `point`) VALUES
-(1, 'aaaa@gmail.com', '山田太郎', '123456789', 'defaulticon.png', 0),
-(2, 'bbbb@gmail.com', '山田二郎', '987654321', 'defaulticon.png', 0),
-(3, 'syu@gmail.com', 'syu', '$2y$10$PK7NxXgaNLgQrR859e7OUeObTIZXuDQy/hXTonukwfGdP.3ioG2/G', 'defaulticon.png', 0),
-(4, 'syuto@gmail.com', 'syuto', '$2y$10$aIhxfvn0qjv2PkjgMJLJ1OJyiizK4n7SWRB8r8gOFWs5RfuSVLigC', 'defaulticon.png', 0),
-(5, 'syuto2@gmail.com', '住谷柊斗', '$2y$10$8BbdFDnxrSjlPyFTqJwBfONGGxqaMxZitiFAoLvLUWhY0604Uk.Im', 'defaulticon.png', 0),
-(6, 'syuto123@gmail.com', 'しゅ', '$2y$10$Kg8EsKH61ygx0ZLDDA61hO.SgLl17OW8pg4Ai7RdWMRbh0CURObKW', 'defaulticon.png', 0),
-(7, 'aa@aaaa', 'a', '$2y$10$YbWEawvqtVgcmpWGpyKx3uy5KJsGtTdH9RfJYE1FSpGBIfwWlGynK', 'defaulticon.png', 0),
-(8, 'a@aaa', 'a', '$2y$10$EyNkK41CsXzKBkN8tN4O3ew8WKBfuaFNXcmrihLqr1gdHcN9tiNXG', 'defaulticon.png', 0),
-(9, 'a@a', 'aaaaa', '$2y$10$LtITLEVD34Sxo2oWysI.DeARRSsGBpqhHlN5pY0kg3a/XIC708Q6S', 'defaulticon.png', 0),
-(10, 'kobe@gmail.com', '神戸太郎', '$2y$10$CpFA6OJqPAzEVxzIAKnn0uYFshMrbSyW0NkxHDxNtXrj/ULp7CgQK', '54969.png', 0),
-(11, 'kobe2@gmail.com', '神戸太郎セカンド', '$2y$10$9JAmK5.d0JOb1XW2UzHU2uXP4YgvOWJXtue/bn84m0aN3gVI0jwPy', 'defaulticon.png', 1),
-(12, 'kobe3@gmail.com', '神戸太郎サード', '$2y$10$c8E45RisebKkvLPl0520JOiZaDZcFseNJO1BfUhkwOfNJlaOeJSpK', 'defaulticon.png', 2),
-(13, 'kishiyutaka0000@gmail.com', 'スギハラユタ', '$2y$10$.qkXEBEPFAmjlRrwdd/GJuHlCTttM30fgKvqOKmTrfzQDSZ3xmrRm', NULL, 0),
-(14, 'kishiyutaka00@gmail.com', '杉原優孝', '$2y$10$Hv7gevZmgfg8JSrvAiAJCuTFbyCheR5Zo9KvpU9iL/38TSfdSaY2a', 'defaulticon.png', 0);
+(17, 'kobe@gmail.com', '神戸太郎', '$2y$10$JWFbzSxepR/x9HlHtFCHHePKTRN1GY6Y0vCgfprYj4b.3c8.3U/Ci', '171642426417鳩.png', 0),
+(18, 'nakai@gmail.com', '中井', '$2y$10$7oO2kd8dfGjEVyaNnresOOzZYR2zUgKWi7dqRDK8ch1vwMVZ.G3Lq', 'defaulticon.png', 0),
+(19, 'masako@gmail.com', '神戸まさこ', '$2y$10$BFchOKZBFtiMGTNSZYBroO4vU/qFUlSsHpYOrhra3JYSrxjsiMiBG', 'otya.jpg', 2),
+(20, 'kyuri0141@gmail.com', '神戸河童', '$2y$10$mvI/yw0gYWkPTBunltOiBefuZ3Es63XaCQRekeSThUqmpwnB18sWK', '171642492720きゅうり.jpg', 0),
+(21, 'aho@gmail.com', '神戸駄長', '$2y$10$xzqkqkn50SIjATofw0vKGu9ShA0EDesm.OlUV6i/Bb.bNBnaxkgX6', '171642513121ダチョウ.jpg', 0),
+(22, 'nagano@gmail.com', '長野太郎', '$2y$10$mt386OOINNtfp2TZvWxFieYcQdGTwMPSlO5Gc.6Vokfv1x5uVj7YS', '171642536722長野県.jpg', 0),
+(23, 'niwaniwaniwaniwatori@gmail.com', '鳥野仁和子', '$2y$10$iJh3.RPgsV8YdgFjm4Dxwu6gxhPgljgYfYWMgHmpdAYn.gk7zJK86', '171642589423ニワトリjpg.jpg', 0),
+(24, 'zinkou@suii.com', '水位仁子', '$2y$10$vgyXk1/1JOFR3LEbSpL0Z.6JTRt8Tu0C8DotnDwJVfFaKR1hOOd9i', '171642749024人口推移.jpg', 0);
 
 --
 -- ダンプしたテーブルのインデックス
@@ -283,31 +221,31 @@ ALTER TABLE `users`
 -- テーブルの AUTO_INCREMENT `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `answer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `answer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- テーブルの AUTO_INCREMENT `article`
 --
 ALTER TABLE `article`
-  MODIFY `article_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `article_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- テーブルの AUTO_INCREMENT `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- テーブルの AUTO_INCREMENT `question`
 --
 ALTER TABLE `question`
-  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- テーブルの AUTO_INCREMENT `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
